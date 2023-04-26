de991af1bca084deebac3ec44134877757cf4446
vishnu-krishnan <vishnuoffcl@gmail.com>
2023-04-26 17:07:05 +0530

    added Student model class

---
+++ StudentDetailsBoot/src/main/java/com/study/model/Student.java
@@ -0,0 +1,66 @@
+package com.study.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String subject;
+	
+	
+
+	public Student() {
+		//Default constructor
+	}
+
+	public Student(Long id, String name, String subject) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.subject = subject;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", subject=" + subject + "]";
+	}
+	
+}
