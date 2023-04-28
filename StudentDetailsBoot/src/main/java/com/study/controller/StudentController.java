c3e26091df5ccd421b10038d151c5d6513ea47d9
vishnu-krishnan <vishnuoffcl@gmail.com>
2023-04-28 12:54:39 +0530

    concised controller layer and conditional check added for updating details in service layer

--- StudentDetailsBoot/src/main/java/com/study/controller/StudentController.java
+++ StudentDetailsBoot/src/main/java/com/study/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.study.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,11 +38,13 @@ public class StudentController {
 	}
 	
 	@PutMapping("/update/{id}")
-	public Student updateStudentDetails(@PathVariable Long id,@RequestBody Student studentDetails) {
-		Student student = getStudentById(id);
-		student.setName(studentDetails.getName());
-		student.setSubject(studentDetails.getSubject());
-		return studentService.saveStudentDetails(student);
+	public ResponseEntity<Student> updateStudentDetails(@PathVariable Long id,@RequestBody Student studentDetails) {
+		Student student = studentService.updateStudentDetails(id, studentDetails);
+		if(student != null) {
+			return ResponseEntity.ok(student);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@DeleteMapping("/delete/{id}")
--- StudentDetailsBoot/src/main/java/com/study/service/StudentService.java
+++ StudentDetailsBoot/src/main/java/com/study/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.study.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -12,14 +13,14 @@ import com.study.repository.StudentRepository;
 
 @Service
 public class StudentService {
-	
+
 	@Autowired
 	private final StudentRepository studentRepository;
-	
+
 	public StudentService (StudentRepository studentRepository) {
 		this.studentRepository = studentRepository;
 	}
-	
+
 	public List<Student> getAllStudents(){
 		return studentRepository.findAll();
 	}
@@ -27,18 +28,29 @@ public class StudentService {
 	public Student getStudentById(Long id) {
 		return studentRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Student is not available"));
 	}
-	
+
 	public Student saveStudentDetails(Student student) {
 		return studentRepository.save(student);
 	}
-	
+
 	public Student updateStudentDetails(Long id, Student studentDetails) {
-		Student student = getStudentById(id);
-		student.setName(studentDetails.getName());
-		student.setSubject(studentDetails.getSubject());
-		return studentRepository.save(student);
+		Optional<Student> studentOptional = Optional.of(getStudentById(id));
+		if(studentOptional.isPresent()) {
+			Student student = studentOptional.get();
+
+			if(studentDetails.getName()!=null)
+				student.setName(studentDetails.getName());
+
+			if(studentDetails.getSubject()!=null)
+				student.setSubject(studentDetails.getSubject());
+
+			return studentRepository.save(student);
+		}
+		else {
+			return null;
+		}
 	}
-	
+
 	public void deleteStudentDetails(Long id) {
 		Student student = getStudentById(id);
 		studentRepository.delete(student);
