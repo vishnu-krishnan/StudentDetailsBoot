c3fa88df78a229338075a09740580b6cfef4879c
vishnu-krishnan <vishnuoffcl@gmail.com>
2023-05-03 13:13:26 +0530

    added lombok to the project and concised Student entity class

--- StudentDetailsBoot/src/main/java/com/study/model/Student.java
+++ StudentDetailsBoot/src/main/java/com/study/model/Student.java
@@ -7,60 +7,38 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name="student")
+@Getter
+@Setter
+@ToString
 public class Student {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
-	@Column(nullable = false)
+	@Column(name = "name")
 	private String name;
 	
-	@Column(nullable = false)
+	@Column(name = "subject")
 	private String subject;
 	
-	
-
 	public Student() {
-		//Default constructor
+		//default
 	}
 
 	public Student(Long id, String name, String subject) {
 		super();
+		System.out.println("student----- "+id+"  "+name+"  "+subject);
 		this.id = id;
 		this.name = name;
 		this.subject = subject;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	@Override
-	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", subject=" + subject + "]";
-	}
+	}	
+	
 	
 }
